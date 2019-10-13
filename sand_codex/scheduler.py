37a5ba84ced5dcdbfce84f349fabc2236250897b
TeddyTrqt <turquet.teddy@gmail.com>
2019-10-13 22:32:07 +0200

    fix: remove useless line

--- sand_codex/scheduler.py
+++ sand_codex/scheduler.py
@@ -7,15 +7,14 @@ class Scheduler:
     def __init__(self, image: str, command: str):
         self.client = docker.from_env()
         self.worker_list = []
-        self.command = "sandcodex_worker"
         self.image = image
         self.command = command
 
-    def add_worker(self):
+    def _add_worker(self):
         worker = Worker(client=self.client, image=self.image, command=self.command)
         worker.launch()
         self.worker_list.append(worker)
 
-    def delete_worker(self):
+    def _delete_worker(self):
         worker = self.worker.pop()
         worker.kill()
