f66e2c40ffece84d167262fb0b832d7138056487
TeddyTrqt <turquet.teddy@gmail.com>
2019-11-20 23:50:41 +0100

    fix: remove useless function

--- sand_codex/utils.py
+++ sand_codex/utils.py
@@ -17,24 +17,3 @@ def text_to_tar_stream(text: str, name: str):
     tar.close()
     tar_stream.seek(0)
     return tar_stream
-
-
-@contextmanager
-def timeout_func(time):
-    # Register a function to raise a TimeoutError on the signal.
-    signal.signal(signal.SIGALRM, raise_timeout)
-    # Schedule the signal to be sent after ``time``.
-    signal.alarm(time)
-
-    try:
-        yield
-    except TimeoutError:
-        pass
-    finally:
-        # Unregister the signal so it won't be triggered
-        # if the timeout is not reached.
-        signal.signal(signal.SIGALRM, signal.SIG_IGN)
-
-
-def raise_timeout(signum, frame):
-    raise TimeoutException()
